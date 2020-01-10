122dca31b3fe9928d1396ec4803f727e4ae50551
HaiVN4 <haivn4@vinid.net>
2020-01-10 17:21:28 +0700

    fix buf cros

--- src/main/java/net/example/BtvnApplication.java
+++ src/main/java/net/example/BtvnApplication.java
@@ -2,6 +2,10 @@ package net.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class BtvnApplication {
@@ -9,4 +13,13 @@ public class BtvnApplication {
 		SpringApplication.run(BtvnApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/api/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "DELETE");
+			}
+		};
+	}
 }
