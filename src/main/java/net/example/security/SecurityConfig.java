4229c55aada48388ddf1c835bd742e5a6bc5f00a
HaiVN4 <haivn4@vinid.net>
2020-01-12 18:57:43 +0700

    .

--- src/main/java/net/example/security/SecurityConfig.java
+++ src/main/java/net/example/security/SecurityConfig.java
@@ -63,7 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST).permitAll()
-                .antMatchers(HttpMethod.GET,"/**").permitAll()
+                .antMatchers(HttpMethod.GET).permitAll()
 //                .antMatchers(HttpMethod.GET, "/product-location").permitAll()
 //                .antMatchers(HttpMethod.POST, "/users/login").permitAll()
 //                .antMatchers(HttpMethod.POST, "/car").permitAll()
